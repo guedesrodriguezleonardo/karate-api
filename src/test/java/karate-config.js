29808614369fbf1bd4cd8aed92bb3dde08d1ccde
lguedesbix <guedesrodriguezleonardo@gmail.com>
2023-05-06 17:57:35 -0300

    Update karate-config.js

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,16 +1,14 @@
 function fn() {
     var env = karate.env;
     karate.log('karate.env system property was:', env);
+    
     if (!env) {
         env = 'dev';
     }
+
     var config = {
         env: env,
-        myVarName: 'hello karate',
         baseUrl: 'https://automationexercise.com/api/'
-    }
-    if (env == 'dev') {
-    } else if (env == 'e2e') {
     }
     return config;
 }
